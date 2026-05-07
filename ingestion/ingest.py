d60b21e8045e30f09655545fed7f10be87490c74
Loganathan Thusharkanth <thusharkanth2002@gmail.com>
2026-05-07 14:08:14 +0530

    FIX: File structure for enterprise-agentic-rag

--- ingestion/ingest.py
+++ ingestion/ingest.py
@@ -1,17 +1,14 @@
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
 
 # 1. Load your dataset
-loader = TextLoader("data/HR policy dataset.md")
+loader = TextLoader("data/hr_policy_dataset.md")
 documents = loader.load()
 
 # 2. Split into chunks
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=500,
-    chunk_overlap=50
-)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 
 docs = text_splitter.split_documents(documents)
 
@@ -20,9 +17,7 @@ embeddings = OllamaEmbeddings(model="llama3")
 
 # 4. Store in Chroma DB
 vectorstore = Chroma.from_documents(
-    docs,
-    embedding=embeddings,
-    persist_directory="rag/vectordb"
+    docs, embedding=embeddings, persist_directory="rag/vectordb"
 )
 
 vectorstore.persist()
